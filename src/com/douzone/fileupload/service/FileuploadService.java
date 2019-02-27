57db4007a9a2dc6081325c08df63ce439d985ae5
bit <bit@DESKTOP-74J9BGV>
2019-02-27 11:11:17 +0900

    Multipart Resolver fileupload완료

--- src/com/douzone/fileupload/service/FileuploadService.java
+++ src/com/douzone/fileupload/service/FileuploadService.java
@@ -1,5 +1,8 @@
 package com.douzone.fileupload.service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Calendar;
 
 import org.springframework.stereotype.Service;
@@ -7,24 +10,42 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileuploadService {
+	//저장위치
+	private static final String SAVE_PATH="/uploads";
+	private static final String URL ="/images";
+	
 	public String restore(MultipartFile multipartFile) {
 		String url = "";
-		if(multipartFile.isEmpty() ) {
-			return url;
-		}//비어있는지 확인
-		
-		String originalFileName = multipartFile.getOriginalFilename();
-		String extName = originalFileName.substring(originalFileName.lastIndexOf(',')+1);//뒤에서 부터 찾아야 확장자 +1?
-		String saveFileName = generateSaveFileName(extName);
-		long fileSize = multipartFile.getSize();
-		
-		System.out.println("################" + originalFileName);
-		System.out.println("################" + extName);
-		System.out.println("################" + saveFileName);
-		System.out.println("################" + fileSize);
-		
+		try {
+			
+			if (multipartFile.isEmpty()) {
+				return url;
+			} // 비어있는지 확인
+
+			String originalFileName = multipartFile.getOriginalFilename();
+			String extName = originalFileName.substring(originalFileName.lastIndexOf(',') + 1);// 뒤에서 부터 찾아야 확장자 +1?
+			String saveFileName = generateSaveFileName(extName);
+			long fileSize = multipartFile.getSize();
+
+			System.out.println("################" + originalFileName);
+			System.out.println("################" + extName);
+			System.out.println("################" + saveFileName);
+			System.out.println("################" + fileSize);
+
+			byte[] fileData = multipartFile.getBytes();
+			OutputStream os = new FileOutputStream(SAVE_PATH+"/"+saveFileName);
+			os.write(fileData);
+			os.close();
+			
+			url = URL + "/"+saveFileName;
+			// 컨트롤러가 알아들을수 있는 exception으로
+			
+		} catch (IOException ex) {
+			new RuntimeException("upload fail");
+		}
 		return url;
 	}
+
 	private String generateSaveFileName(String extName) {
 		String fileName = "";
 		Calendar calendar = Calendar.getInstance();
@@ -35,8 +56,8 @@ public class FileuploadService {
 		fileName += calendar.get(Calendar.MINUTE);
 		fileName += calendar.get(Calendar.SECOND);
 		fileName += calendar.get(Calendar.MILLISECOND);
-		fileName += ("."+extName);
-		
+		fileName += ("." + extName);
+
 		return fileName;
 	}
 }
